e1a306e9818d47e222b4638ddf8a420127c3d36c
SandActor <luka.timohin@yandex.ru>
2025-05-19 16:20:00 +0500

    4 шаг

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,18 +8,12 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-V, --version', 'output the version number')
+  .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const options = program.opts();
-    const diff = genDiff(filepath1, filepath2, options.format);
+    const diff = genDiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
-  })
-  .addHelpText('after', `
-Examples:
-  $ gendiff file1.json file2.json
-  $ gendiff file1.yml file2.yml --format plain
-  $ gendiff file1.json file2.json -f json`);
+  });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-export default function genDiff(filepath1, filepath2, format = 'stylish') {
-  return `Difference between ${filepath1} and ${filepath2} in ${format} format`;
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
+const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+const parse = (data) => JSON.parse(data);
+
+const buildDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+
+  return sortedKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      return `    ${key}: ${data1[key]}`;
+    }
+    return [
+      `  - ${key}: ${data1[key]}`,
+      `  + ${key}: ${data2[key]}`
+    ].join('\n');
+  }).join('\n');
+};
+
+export default function genDiff(filepath1, filepath2) {
+  const file1Content = readFile(filepath1);
+  const file2Content = readFile(filepath2);
+  
+  const data1 = parse(file1Content);
+  const data2 = parse(file2Content);
+  
+  const diff = buildDiff(data1, data2);
+  return `{\n${diff}\n}`;
 }
