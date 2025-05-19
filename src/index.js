c98435a3d4706685d9b70ad4f0a79959e1689f4b
SandActor <luka.timohin@yandex.ru>
2025-05-19 15:58:02 +0500

    2 шаг

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export default function genDiff(filepath1, filepath2, format = 'stylish') {
-  // Здесь будет логика сравнения файлов с учетом формата
   return `Difference between ${filepath1} and ${filepath2} in ${format} format`;
 }
