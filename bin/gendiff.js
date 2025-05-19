eb289971768741c05ea33ae152b2add9f028cf44
SandActor <luka.timohin@yandex.ru>
2025-05-19 15:57:21 +0500

    2 шаг

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,11 +9,17 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+    const options = program.opts();
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
-  });
+  })
+  .addHelpText('after', `
+Examples:
+  $ gendiff file1.json file2.json
+  $ gendiff file1.yml file2.yml --format plain
+  $ gendiff file1.json file2.json -f json`);
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default function genDiff(filepath1, filepath2) {
-  // Здесь будет логика сравнения файлов
-  return 'Difference between files will be here';
+export default function genDiff(filepath1, filepath2, format = 'stylish') {
+  // Здесь будет логика сравнения файлов с учетом формата
+  return `Difference between ${filepath1} and ${filepath2} in ${format} format`;
 }
