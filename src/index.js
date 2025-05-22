93134a35ed90e9e3d5d1dea71d9c06e8a65861c6
SandActor <luka.timohin@yandex.ru>
2025-05-22 16:23:35 +0500

    1.1.9

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import yaml from 'js-yaml';
 import getFormatter from './formatters/index.js';
 
 const getFixturePath = (filename) => {
-  const testPath = path.join('./__fixtures__', filename);
+  const testPath = path.join('', filename);
   readFileSync(testPath);
   return testPath;
 };
