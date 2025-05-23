d95573ac8502973a79b9f41554b68fbe71f068ec
SandActor <luka.timohin@yandex.ru>
2025-05-23 18:35:29 +0500

    1.3.4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url'
 import path from 'path'
 import fs from 'fs'
-import genDiff from '../src/index.js'
+import genDiff from '../src/index'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
