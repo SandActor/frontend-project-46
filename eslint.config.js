95d300f136823d7b27f9a6a44d7ad5ef53030fe8
SandActor <luka.timohin@yandex.ru>
2025-05-24 16:59:36 +0500

    1.4.3

--- eslint.config.js
+++ eslint.config.js
@@ -3,29 +3,23 @@ import globals from 'globals'
 import pluginReact from 'eslint-plugin-react'
 
 export default [
-    js.configs.recommended,
-    {
-        files: ['**/*.{js,mjs,cjs,jsx}'],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.es2021
-            }
-        }
-    },
-    {
-        files: ['**/*.{jsx}'],
-        ...pluginReact.configs.recommended,
-        settings: {
-            react: {
-                version: 'detect'
-            }
-        }
-    },
-    {
-        rules: {
-            'react/jsx-uses-react': 'error',
-            'react/jsx-uses-vars': 'error'
-        }
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      }
     }
+  },
+  {
+    files: ['**/*.{jsx}'],
+    ...pluginReact.configs.recommended,
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
 ]
