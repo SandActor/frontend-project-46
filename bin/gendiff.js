e1d74e4edaf0f6a3edb783eb0868b653904f6f85
SandActor <luka.timohin@yandex.ru>
2025-05-24 21:00:45 +0500

    1.7.2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
-import { Command } from 'commander'
-import { argv } from 'process'
-import gendiff from '../src/index.js'
 
-const program = new Command()
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
 
 program
-  .version('1.0.0')
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format(default: "stylish")')
-  .action((filepath1, filepath2, option) => {
-    console.log(gendiff(filepath1, filepath2, option.format))
-  })
+  .option('-f, --format <type>', 'output format (default: "stylish")', 'stylish')
+  .helpOption('-h, --help', 'display help for command')
+  .action((filepath1, filepath2) => {
+    const options = program.opts();
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
-program.parse(argv)
+program.parse(process.argv);
